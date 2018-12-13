9cef08ef0cf04d91d087686869c43345991d7bca
YazeedHammad <yazeedhammad91@gmail.com>
2018-12-13 10:54:30 +0200

    (feat)ContactUs Schema was added ,database connected and checked

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: true
 }))
-// mongoose.connect('mongodb://BCT:gg835864@ds127842.mlab.com:27842/blackcodeteam')
+mongoose.connect('mongodb://BCT:gg835864@ds127842.mlab.com:27842/blackcodeteam')
 // mongoose.connect('mongodb://ozil:ozil123@ds131814.mlab.com:31814/ozil')
 
 mongoose.Promise = global.Promise;
